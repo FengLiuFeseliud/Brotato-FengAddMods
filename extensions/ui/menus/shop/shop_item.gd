3fcb798127faef34806c78dc7934dd19c5a78a18
FengLiuFeseliud <bd233mc666@gmail.com>
2026-08-28 18:03:01 +0800

    fix set_shop_item  set_material_icon bug

--- extensions/ui/menus/shop/shop_item.gd
+++ extensions/ui/menus/shop/shop_item.gd
@@ -1,6 +1,9 @@
 extends ShopItem
 
 
+var material_ui_icon = load("res://items/materials/material_ui.png")
+
+
 var effect_fengliu_stats_stop = Keys.generate_hash("fengliu_stats_stop")
 
 
@@ -14,7 +17,9 @@ func set_shop_item(p_item_data: ItemParentData, p_wave_value: int = RunData.curr
         return
     
     var effect = effects[0]
+    # 金币足够且允许金币购买：保持金币显示，并还原金币图标（防止上一帧属性图标残留）
     if RunData.get_player_gold(player_index) >= value and effect[2]:
+        _button.set_material_icon(material_ui_icon, Utils.GOLD_COLOR)
         return
 
     var material_icon: Image = ItemService.get_stat_icon(effect[0]).get_data()
@@ -22,5 +27,5 @@ func set_shop_item(p_item_data: ItemParentData, p_wave_value: int = RunData.curr
     texture.create_from_image(material_icon)
     # 切换为属性代付图标与价格
     _button.set_material_icon(texture)
-    _button.set_value(int(ceil(value / float(effect[1]))), RunData.get_player_currency(player_index))
+    _button.set_value(int(ceil(value / float(effect[1]))), int(RunData.get_stat(effect[0], player_index)))
     
